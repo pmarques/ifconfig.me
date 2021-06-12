66305befa137caa161b4d033103eecb34df093aa
Patrick F. Marques <patrickfmarques@gmail.com>
2021-06-12 21:51:48 +0100

    Add option to log into file
    This is just a simple implementation to write directly to files since we
    ware using a static build without sheel

--- app/main.go
+++ app/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/pmarques/ifconfig.me/handlers/ip"
 )
@@ -18,9 +19,22 @@ type Error struct {
 func main() {
 	listenPort := flag.Int("port", 80, "The port to bind http server")
 	listenAddr := flag.String("addr", "", "The addr to bind http server")
+	logFileName := flag.String("log-filename", "", "The filename where to write logs")
 
 	// Parse command line arguments
 	flag.Parse()
+
+	if *logFileName != "" {
+		log.Println(`Using "` + *logFileName + `" file for logs`)
+		file, err := os.OpenFile(*logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer file.Close()
+
+		log.SetOutput(file)
+	}
 
 	// Log all the other requests and return 404
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
@@ -43,6 +57,7 @@ func main() {
 	http.HandleFunc("/ip", ip.Handler)
 
 	bindAddr := fmt.Sprintf("%s:%d", *listenAddr, *listenPort)
+	log.Println(`Start listenning at "` + bindAddr + `"`)
 	fmt.Println(`Start listenning at "` + bindAddr + `"`)
 
 	log.Fatal(http.ListenAndServe(bindAddr, nil))
