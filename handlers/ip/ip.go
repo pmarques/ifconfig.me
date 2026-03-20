524e494ef22009f76e5d8fe2901dc5a631b901eb
Patrick Marques <pmarques@users.noreply.github.com>
2026-03-20 07:30:15 +0000

    Update nosemgrep comment for XSS audit rule

--- handlers/ip/ip.go
+++ handlers/ip/ip.go
@@ -64,7 +64,7 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 			"Content-type", "application/xml",
 		)
 
-		// nosemgrep: lang.security.audit.xss.no-io-writestring-to-responsewriter.no-io-writestring-to-responsewriter
+		// nosemgrep: go.lang.security.audit.xss.no-io-writestring-to-responsewriter.no-io-writestring-to-responsewriter
 		io.WriteString(res, xml.Header)
 		enc := xml.NewEncoder(res)
 		enc.Indent("  ", "    ")
